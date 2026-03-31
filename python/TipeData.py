b7365880d6c830611de8d4c83c1d4a8cfaa7b847
RyukiShinji <rothgaberruscetti357@gmail.com>
2026-03-31 17:22:55 +0800

    Add files via upload

---
+++ python/TipeData.py
@@ -0,0 +1,4 @@
+# a = 10, a adalah variabel dengan nilai 10
+
+# tipe data: angka satuan yang gak ada
+# koma nya (integer)
---
+++ python/py1.py
@@ -0,0 +1,6 @@
+local = 12, 13, 14
+a = 12
+b = 13
+c = 14
+
+print(a, b, c, "hello world")
